c7b90bea5251ee9d9d3db8425cc6fd4f469f18f3
Pablo Ruan <pablommoc@gmail.com>
2022-03-29 20:24:01 -0300

    first commit

---
+++ fylo-dark-theme-landing-page-master/script.js
@@ -0,0 +1,9 @@
+
+const btnMenu = document.querySelector('#menu-btn')
+
+function toggleMenu() {
+    const nav = document.querySelector('#navMobile')
+    nav.classList.toggle('true')
+} 
+
+btnMenu.addEventListener('click', toggleMenu)
